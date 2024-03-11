8b96237fb27276d265be4a63c0e920c082e064f3
iTsGiuse <giuse.amato@icloud.com>
2024-03-11 16:15:38 +0100

    feat: add script SNACK3

---
+++ snack3/js/script.js
@@ -0,0 +1,23 @@
+/* Crea un array vuoto.
+  Chiedi per 6 volte all’utente di inserire un numero,
+  se è dispari inseriscilo nell’array. */
+
+/* CREAZIONE ARRAY */
+const dispari = [];
+
+/* CREAZIONE CICLO */
+for (let i = 0; i < 6; i++) {
+
+        /* INSERIMENTO NUMERI FROM INPUT */
+        let number = parseInt(prompt('inserisci un numero'));
+        console.log(number);
+    
+        /* VERIFICA SE IL NUMERO E' DISPARI E AGGIUNGILO NELL'ARRAY */
+        if (number % 2 === 1) {
+            dispari.push(number);
+        }
+
+}
+
+/* MOSTRA ARRAY DISPARI IN CONSOLE */
+console.log(dispari);
