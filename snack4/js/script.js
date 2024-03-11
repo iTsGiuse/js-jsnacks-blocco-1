a853386a128c2f01645312f8bc725fafa6cbb402
iTsGiuse <giuse.amato@icloud.com>
2024-03-11 17:17:25 +0100

    fix: fix code SNACK4

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -1,23 +1,35 @@
 /* Calcola la somma e la media dei primi 10 numeri. */
+
 /* DICHIARAZIONI VARIABILI */
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let sum = 0;
 let media = 0;
 
 /* CICLO */
-for (let i = 0; i <= 10; i++ ) {
-
-    /* INSERISCI I NUMERI */
-    let number = parseInt(prompt('Inserisci un numero'));
-    console.log('il numero inserito è' + '  ' + number);
-
-    /* FAI LA SOMMA PROVVISORIA */ 
-    sum = sum + number; 
+for (let i = 0; i < 10; i++) {
+    sum = sum + numbers[i]; 
     console.log('La somma per ora è' + '  ' + sum);
-
 }
-/* FAI LA SOMMA*/ 
+
+/* FAI LA SOMMA */ 
 console.log('La somma è' + '  ' + sum);
 
 /* FAI LA MEDIA */ 
 media = sum / 10 ;
 console.log('La media è' + '  ' + media);
+
+
+
+/* CICLO */
+/* for (let i = 0; i <= 10; i++ ) { */
+
+    /* INSERISCI I NUMERI */
+    /* let number = parseInt(prompt('Inserisci un numero')); */
+    /* console.log('il numero inserito è' + '  ' + number); */
+
+    /* FAI LA SOMMA PROVVISORIA */ 
+    /* sum = sum + number; */ 
+    /* console.log('La somma per ora è' + '  ' + sum); */
+
+/* } */
